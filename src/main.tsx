49025f97c684ec4d02d1870454c9d136bba88cf5
Abnernoa97 <soloaplicaciones97@gmail.com>
2026-09-24 01:59:09 -0600

    Load event hub styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './styles.css';
 import './events.css';
+import './event-hub.css';
 import './calendar.css';
 import './sheet.css';
 import './reminders.css';
